ef87e1274c44ff98e3d041e5db03f875eae49050
sineTlsl <limsieun0313@gmail.com>
2023-12-10 19:44:06 +0900

    style: 카카오 버튼 css 설정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  endOfLine: 'auto',
 };
